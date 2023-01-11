5c11029b36260d12665d67f12e4a111bee8e0d0f
GeorgeKryptonian <georgekryptonian@gmail.com>
2023-01-11 16:52:11 +0100

    Updated gulp tasks and other things

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,23 @@
 const gulp = require('gulp');
+const replace = require('gulp-replace');
 const htmlmin = require('gulp-htmlmin');
 const cssmin = require('gulp-cssmin');
 
 
-const minifyHTML = () => gulp.src('src/index.html')
+const transformHTML = () => gulp.src('src/**/*.html')
+    .pipe(replace(/\.\.\/.*\/output\.css/, 'output.css'))
     .pipe(htmlmin({
         collapseWhitespace: true,
         ignoreCustomFragments: [/<pre[\s\S]*pre>/]
     }))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('docs'))
 
-const minifyCSS = () => gulp.src('dist/**/*.css')
+const transformCSS = () => gulp.src('docs/**/*.css')
     .pipe(cssmin())
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('docs'))
 
 const copyToDist = () => gulp.src(['src/**', '!src/input.css', '!src/index.html'])
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('docs'));
 
 
-exports.default = gulp.series(copyToDist, minifyHTML, minifyCSS);
+exports.default = gulp.series(copyToDist, transformHTML, transformCSS);
